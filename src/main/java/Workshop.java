e516b48025bffc5763173b27c6ad672f32fcc539
Lina <sanchezbarreralinamaria702@gmail.com>
2026-09-05 09:36:49 -0500

    resvisionrepositorio

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -17,9 +17,9 @@ public class Workshop {
     public int mayorDeTresNumeros(int a, int b, int c) {
         // TODO: Implementar el método para retornar el mayor de los tres números enteros.
         // Ejemplo: Si a = 3, b = 7, y c = 5, el resultado debería ser 7.
-      if (a>= b && a >=c) return a;
-    if (b>=c) return b;  
-      return c;
+       if (a>= b && a >=c) return a;
+       if (b>=c) return b;  
+       return c;
     }
 
     // Método que retorna la tabla de multiplicar de un número
