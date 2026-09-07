c3654af971a837d56d917c51332fe8348fb92a9c
Lina <sanchezbarreralinamaria702@gmail.com>
2026-09-07 13:34:38 -0500

    correo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -330,7 +330,8 @@ public class Workshop {
     public boolean validarCorreoElectronico(String correo) {
         // TODO: Implementar el método para validar un correo electrónico.
         // Ejemplo: Si correo = "test@example.com", el resultado debería ser true.
-        return false;
+        if (correo == null) return false;
+        return correo.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
     }
 
     // Método que calcula el promedio de una lista de números
