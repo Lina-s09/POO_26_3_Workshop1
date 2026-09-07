8e517622f8a62f8a67dc9a61d7732150efede4a3
Lina <sanchezbarreralinamaria702@gmail.com>
2026-09-07 13:07:26 -0500

    unir arreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -211,7 +211,16 @@ public class Workshop {
     public int[] combinarArreglos(int[] arreglo1, int[] arreglo2) {
         // TODO: Implementar el método para combinar dos arreglos en uno solo.
         // Ejemplo: Si arreglo1 = [1, 2, 3, 4, 5] y arreglo2 = [6, 7, 8], el resultado debería ser [1, 2, 3, 4, 5, 6, 7, 8].
-        return new int[0];
+        int po = 0;
+        for (int u = 0; u < arreglo1.length; u++) {
+            resultado[po] = arreglo1[u];
+            po++;
+        }
+        for (int u = 0; u < arreglo2.length; u++) {
+            resultado[po] = arreglo2[u];
+            po++;
+        }
+        return resultado;
     }
 
     // Método que rota un arreglo n posiciones
